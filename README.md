dfa98dc7fe3457cc8b32a36be8b215f18ead55ec
Sergio Martino <sergio.martino@modo.com.ar>
2024-02-26 00:22:42 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Example Journal APP
+
+Ejemplo de aplicación usando Material UI
+
+## Installations
+
+* React + TypeScript + Vite (```yarn create vite```)
+* react-router-dom (```yarn add react-router-dom```)
+* Material UI + Emotion React + Emotion Styled (```yarn add @mui/material @emotion/react @emotion/styled```)
+* Icons (```yarn add @mui/icons-material```)
+* Robot fonts (Vía link tags on index.html)
---
+++ src/journal/pages/JournalPage.tsx
@@ -0,0 +1,10 @@
+import { Typography } from '@mui/material';
+import { JournalLayout } from '../layout/JournalLayout';
+
+export const JournalPage = () => {
+  return (
+    <JournalLayout>
+      <Typography>Labore ut cillum deserunt labore deserunt quis nulla culpa esse fugiat dolor adipisicing enim. Deserunt nulla consequat quis in dolore nulla sint. Consectetur veniam esse incididunt ex enim est eiusmod quis. Incididunt exercitation veniam ullamco ut. Eiusmod qui sunt incididunt et excepteur. Ut quis occaecat elit incididunt qui culpa do ullamco nulla pariatur nulla. Pariatur irure amet ut aute aliquip nisi mollit ad nulla commodo.</Typography>
+    </JournalLayout>
+  )
+}
